d9eff2024e8a5589164e74f2a51a1d9e5681833a
Reza Fauzi Zisiwa Duha <rezafauzizisiwaduha@gmail.com>
2021-05-27 11:04:40 +0700

    add AllPresenter.kt

---
+++ app/src/main/java/com/belajarkuy/app/data/presenter/AllPresenter.kt
@@ -0,0 +1,79 @@
+package com.belajarkuy.app.data.presenter
+
+import android.content.Context
+import com.belajarkuy.app.data.model.AuthRequest
+import com.belajarkuy.app.data.model.ModuleRequest
+import com.belajarkuy.app.data.network.ApiConfig
+import com.belajarkuy.app.view.GeneralView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AllPresenter(private val view: GeneralView, private val context: Context) {
+
+    private val api = ApiConfig.getApiService(context)
+
+    fun continueWithGoogle(authRequest: AuthRequest) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val request = api.login(authRequest)
+            try {
+                val response = request
+                view.success(response)
+            }catch (e: Exception) {
+                view.error(e)
+            }
+        }
+    }
+
+    fun getProgress(userId: Int) {
+        view.showLoading()
+        CoroutineScope(Dispatchers.Main).launch {
+            val request = api.getProgress(userId)
+            try {
+                val response = request
+                view.success(response)
+            } catch (e: Exception) {
+                view.error(e)
+            }
+        }
+    }
+
+    fun getAllModule() {
+        view.showLoading()
+        CoroutineScope(Dispatchers.Main).launch {
+            val request = api.getAllModule()
+            try {
+                val response = request
+                view.success(response)
+            } catch (e: Exception) {
+                view.error(e)
+            }
+        }
+    }
+
+    fun getModuleById(moduleId: Int) {
+        view.showLoading()
+        CoroutineScope(Dispatchers.Main).launch {
+            val request = api.getModuleById(moduleId)
+            try {
+                val response = request
+                view.success(response)
+            } catch (e: Exception) {
+                view.error(e)
+            }
+        }
+    }
+
+    fun submitAnswer(moduleId: Int, moduleRequest: List<ModuleRequest>) {
+        view.showLoading()
+        CoroutineScope(Dispatchers.Main).launch {
+            val request = api.submitAnswer(moduleId, moduleRequest)
+            try {
+                val response = request
+                view.success(response)
+            } catch (e: Exception) {
+                view.error(e)
+            }
+        }
+    }
+}
