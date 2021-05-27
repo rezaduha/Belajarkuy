294ac102253a7857422729010a93d4dcbb0171f0
Reza Fauzi Zisiwa Duha <rezafauzizisiwaduha@gmail.com>
2021-05-27 08:43:35 +0700

    add main screen

--- app/src/main/java/com/belajarkuy/app/MainActivity.kt
+++ app/src/main/java/com/belajarkuy/app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.belajarkuy.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.belajarkuy.app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHost.navController
+        val navView: BottomNavigationView = binding.navView
+
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_module, R.id.navigation_profile
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
