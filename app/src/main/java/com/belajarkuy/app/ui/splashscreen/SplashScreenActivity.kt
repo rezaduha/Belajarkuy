5e8b7e1e5de052427b356eb10e84a9d3915db4d1
Reza Fauzi Zisiwa Duha <rezafauzizisiwaduha@gmail.com>
2021-05-27 08:51:10 +0700

    add splash screen

---
+++ app/src/main/java/com/belajarkuy/app/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.belajarkuy.app.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.belajarkuy.app.MainActivity
+import com.belajarkuy.app.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
