bde258bf1d5ffe321b0d114276ffd9ef0882d0db
Reza Fauzi Zisiwa Duha <rezafauzizisiwaduha@gmail.com>
2021-06-01 20:29:36 +0700

    coloring card view

--- app/src/main/java/com/belajarkuy/app/ui/module/ModuleAdapter.kt
+++ app/src/main/java/com/belajarkuy/app/ui/module/ModuleAdapter.kt
@@ -1,11 +1,13 @@
 package com.belajarkuy.app.ui.module
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.belajarkuy.app.R
 import com.belajarkuy.app.data.model.ModulesItem
 import com.belajarkuy.app.databinding.ItemModuleBinding
+import kotlin.random.Random
 
 class ModuleAdapter(
     private val modules: MutableList<ModulesItem>,
@@ -23,10 +25,12 @@ class ModuleAdapter(
 
     inner class ModuleViewHolder(private val binding: ItemModuleBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(data: ModulesItem, listener: Listener) {
+            val color = Color.argb(255, Random.nextInt(210), Random.nextInt(210), Random.nextInt(210))
             with(binding) {
                 tvSubject.text = data.subject
                 tvTotalQuestion.text = itemView.context.resources.getString(R.string.total_questions, data.totalQuestions.toString())
                 tvDuration.text = itemView.context.resources.getString(R.string.duration, data.duration.toString())
+                cvModule.setCardBackgroundColor(color)
             }
             itemView.setOnClickListener {
                 listener.onClick(data)
