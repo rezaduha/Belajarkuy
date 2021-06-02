d38607bcdf8f5b55e45a94bf9571aa885145587b
Reza Fauzi Zisiwa Duha <rezafauzizisiwaduha@gmail.com>
2021-06-03 04:05:55 +0700

    fix service request

--- app/src/main/java/com/belajarkuy/app/data/network/ApiService.kt
+++ app/src/main/java/com/belajarkuy/app/data/network/ApiService.kt
@@ -8,21 +8,24 @@ interface ApiService {
     @POST("auth")
     suspend fun login(@Body authRequest: AuthRequest): AuthRequest
 
-//    @GET("/competency/{user_id}")
-//    suspend fun getCompetency(@Path("user_id") userId: Int): CompetencyResponse
-//
-//    @GET("/modules")
-//    suspend fun getAllModule(): ModuleResponse
-//
-//    @GET("/module/{module_id}")
-//    suspend fun getModuleById(@Path("module_id") moduleId: Int): DetailModuleResponse
-//
-//    @GET("/module/{user_id}/{subject}")
-//    suspend fun getRecommendationBySubject(@Path("user_id") userId: Int, @Path("subject") subject: String): DetailModuleResponse
+    @GET("competency/{user_id}")
+    suspend fun getCompetency(@Path("user_id") userId: Int): CompetencyResponse
 
-    @POST("/module/{module_id}")
+    @GET("module/list")
+    suspend fun getAllModule(): ModuleResponse
+
+    @GET("module/{module_id}")
+    suspend fun getModuleById(@Path("module_id") moduleId: Int): DetailModuleResponse
+
+    @GET("module/{user_id}/{subject}")
+    suspend fun getRecommendationBySubject(
+        @Path("user_id") userId: Int,
+        @Path("subject") subject: String
+    ): DetailModuleResponse
+
+    @POST("module/{user_id}")
     suspend fun submitAnswer(
-        @Path("module_id") moduleId: Int,
+        @Path("user_id") userId: Int,
         @Body moduleRequest: List<ModuleRequest>
     ): ModuleRequest
 
@@ -32,23 +35,4 @@ interface ApiService {
         @Query("category") category: String = "science",
         @Query("apiKey") apiKey: String = "4b7abda9e69f41579a797be412b6fd7e",
     ): NewsResponse
-
-
-    // empty competency endpoint = 2176bcc5-3545-46f6-8e77-a3fb9031f7b2
-    // not empty endpoint = e345a71a-e6e6-4246-b234-e9ef2b6b579b
-    // just for testing =====================
-    @GET("e345a71a-e6e6-4246-b234-e9ef2b6b579b")
-    suspend fun getCompetency(): CompetencyResponse
-
-    @GET("d5845f7e-b404-4b5a-a54b-88b70207c41d")
-    suspend fun getAllModule(): ModuleResponse
-
-    @GET("faa85def-6274-4a18-ad62-c380f875ec63")
-    suspend fun getModuleById(): DetailModuleResponse
-
-    @GET("/module/{user_id}/{subject}")
-    suspend fun getRecommendationBySubject(
-        @Path("user_id") userId: Int,
-        @Path("subject") subject: String
-    ): DetailModuleResponse
 }
